68bbed4eaac71a2f4fe2328b05e49ad21839d040
Walter Kimaro <wakyj07@gmail.com>
2020-10-15 16:54:04 +0300

    Adapt UIElement to match new structure of class ui elements

---
+++ src/components/BCUIElement/index.jsx
@@ -0,0 +1,68 @@
+import { h } from 'vue';
+
+export default {
+    props:{
+        element: Object
+    },
+
+    render(){
+        const { element, $emit } = this;
+        const { options, component } = element;
+
+        function renderActionButtons(){
+            let className = "component-editor-buttons"; 
+            className += component === 'bc-separator' ? 'delete-only' : '';
+
+            return (
+                <div class={className}>
+                    <button class="component-editor-button"
+                        onClick={() => $emit('editElement', element)}>
+                        <svg fill="#f18f16" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                    </button>
+                    <button class="component-editor-button"
+                        onClick={() => $emit('removeElement', element.id)}>
+                        <svg fill="#e04b2a" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                    </button>
+                </div>
+            );
+        }
+
+        let baseClass = 'blogpost-section-wrapper';
+        if((component === 'bc-image' || component === 'bc-youtube-video') && options){
+            if(options.width === 'wide')
+                baseClass += ' wide-image';
+            else if(options.width === 'full')
+                baseClass += ' full-image';
+        }
+
+        const uiComponent = new window._Articulate.uiElements[component](options);
+
+        return (
+            <div class={baseClass}>
+                <div class="component-wrapper" style="position: relative;">
+                    { renderActionButtons() }
+
+                    {  h( "div", { innerHTML: uiComponent.render() }) }
+                </div>
+            </div>
+        );
+    },
+
+    data: function(){
+        return{
+            component: null,
+            options: {}
+        }
+    },
+
+    watch: {
+        element:{
+            immediate: true,
+            deep: true,
+            handler: function(newValue){
+                this.component = newValue.component;
+                this.options = newValue.options;
+            }
+        }
+    }
+}
